1cd1d9b705f92e1c802ee2d2048db6460d0bc7d5
Muhammad Dzaka Murran Rusid <99970931+dzkmrn@users.noreply.github.com>
2024-08-08 11:41:20 +0700

    Create UI Dashboard and BottomNavbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,26 +50,29 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-/*    implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.5.1")
-
-    //retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
-    //ui
-    //implementation(libs.androidx.datastore.preferences)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.github.Ferfalk:SimpleSearchView:0.2.1")
-    //implementation(libs.ucrop)
-    implementation("androidx.multidex:multidex:2.0.1")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")*/
+
+
+    /*    implementation("androidx.activity:activity:1.9.0")
+        implementation("androidx.fragment:fragment-ktx:1.5.1")
+
+        //retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+        //ui
+        //implementation(libs.androidx.datastore.preferences)
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+        implementation("androidx.activity:activity-ktx:1.9.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+        implementation("com.github.bumptech.glide:glide:4.16.0")
+        implementation("androidx.core:core-splashscreen:1.0.1")
+        implementation("de.hdodenhof:circleimageview:3.1.0")
+        implementation("com.github.Ferfalk:SimpleSearchView:0.2.1")
+        //implementation(libs.ucrop)
+        implementation("androidx.multidex:multidex:2.0.1")
+        implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")*/
 }
