a7f68e063a7e125c7dc22d9028a045d2ba1e21a4
Muhammad Dzaka Murran Rusid <99970931+dzkmrn@users.noreply.github.com>
2024-08-09 08:35:57 +0700

    Creating Settings and Mutation UI

---
+++ app/src/main/java/com/bs/sriwilis/ui/settings/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.bs.sriwilis.ui.settings
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bs.sriwilis.R
+import com.bs.sriwilis.databinding.ActivityChangeProfileBinding
+import com.bs.sriwilis.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
